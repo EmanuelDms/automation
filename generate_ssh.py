71610356b8ee62a9d967f94dbd94b270dd50733e
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-01-24 20:24:16 -0300

    chore: add suport for pacman

--- generate_ssh.py
+++ generate_ssh.py
@@ -5,15 +5,28 @@ import sys
 
 try:
     email = sys.argv[1]
+    option = sys.argv[2]
 except IndexError:
     print("You must past your email on first argument.")
 else:
-    instructions = f"""
-    sudo apt install xclip -y;
-    echo "xclip was successfully installed.";
-    ssh-keygen -t rsa -b 4096 -C "{email}";
-    echo "SSH key was successfully generated to {email}.";
-    xclip -sel clip < ~/.ssh/id_rsa.pub;
-    echo "Your SSH key is on your clipboard... just paste it!"
-    """
+    # apt method
+    if (option == 0):
+        instructions = f"""
+        sudo apt install xclip -y;
+        echo "xclip was successfully installed.";
+        ssh-keygen -t rsa -b 4096 -C "{email}";
+        echo "SSH key was successfully generated to {email}.";
+        xclip -sel clip < ~/.ssh/id_rsa.pub;
+        echo "Your SSH key is on your clipboard... just paste it!"
+        """
+    # pacman method
+    else:
+        instructions = f"""
+        sudo pacman -S xclip;
+        echo "xclip was successfully installed.";
+        ssh-keygen -t rsa -b 4096 -C "{email}";
+        echo "SSH key was successfully generated to {email}.";
+        xclip -sel clip < ~/.ssh/id_rsa.pub;
+        echo "Your SSH key is on your clipboard... just paste it!"
+        """
     os.system(instructions)
